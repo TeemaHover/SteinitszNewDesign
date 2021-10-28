43c87a2b6fb6bfc10f1c839250e92c4dc9fdebd4
Teema <temuulenhover@gmail.com>
2021-10-29 06:47:51 +0800

    ASSETS NEMELT

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function showMenu() {
+    var element = document.getElementById("navList");
+    element.classList.toggle("open");
+}
--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,24 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-
     slidesPerView: 4,
+    breakpoints: {
+        // when window width is >= 320px
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+
+        // when window width is >= 640px
+        600: {
+            slidesPerView: 4,
+            spaceBetween: 40
+        }
+    },
+    autoplay: {
+        delay: 2000,
+    },
+
     pagination: {
         el: '.swiper-pagination',
     },
