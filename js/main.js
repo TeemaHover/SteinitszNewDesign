acd41c51b440c5c773543f753f2032f58e63c49f
Teema <temuulenhover@gmail.com>
2021-11-12 17:01:50 +0800

    shine design zasalt

--- js/main.js
+++ js/main.js
@@ -2,3 +2,36 @@ function showMenu() {
     var element = document.getElementById("navList");
     element.classList.toggle("open");
 }
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > $(window).height()) {
+            $(".navigation").css({ "background-color": "black" });
+        } else {
+            $(".navigation").css({ "background-color": "transparent" });
+        }
+
+    })
+})
+
+$(function() {
+    $(document).scroll(function() {
+        var $nav = $(".navigation");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+$(function() {
+    "use strict";
+
+    $(".enviromentBox img").click(function() {
+        var $src = $(this).attr("src");
+        $(".show").fadeIn();
+        $(".img-show img").attr("src", $src);
+    });
+
+    $("span, .overlay").click(function() {
+        $(".show").fadeOut();
+    });
+
+});
--- js/swiper.js
+++ js/swiper.js
@@ -1,7 +1,38 @@
+const Homeswiper = new Swiper('.Homeswiper', {
+
+    direction: 'horizontal',
+    loop: true,
+
+    autoplay: {
+        delay: 2000,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+
+});
+
+document.getElementById("Homeswiper").onmouseover = function() { mouseOver() };
+document.getElementById("Homeswiper").onmouseout = function() { mouseOut() };
+
+const aboutsiper = new Swiper('.swiperAbout', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+})
+
+
 const swiper = new Swiper('.swiper', {
 
     direction: 'horizontal',
-    slidesPerView: 4,
+
     breakpoints: {
 
         300: {
@@ -22,9 +53,6 @@ const swiper = new Swiper('.swiper', {
         delay: 2000,
     },
 
-    pagination: {
-        el: '.swiper-pagination',
-    },
 
     navigation: {
         nextEl: '.swiper-button-next',
@@ -36,6 +64,20 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-scrollbar',
     },
 });
+
+document.getElementById("swiper").onmouseover = function() { mouseOver() };
+document.getElementById("swiper").onmouseout = function() { mouseOut() };
+
+function mouseOver() {
+    swiper.autoplay.stop();
+    Homeswiper.autoplay.stop();
+}
+
+function mouseOut() {
+    swiper.autoplay.start();
+    Homeswiper.autoplay.start();
+}
+
 const swiper2 = new Swiper('.swiperTeachers', {
 
     autoHeight: true,
